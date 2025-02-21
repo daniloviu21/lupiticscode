89d742c76d0341eea1b04aa4b86bdfaeeae9f8e2
karl262 <carlins502@gmail.com>
2025-02-21 09:57:51 -0600

    Fix: Edit layout-page component for routes.

--- src/app/heroes/pages/layout-page/layout-page.component.ts
+++ src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -4,15 +4,15 @@ import { url } from 'inspector';
 @Component({
   selector: 'app-layout-page',
   standalone: false,
-  
+
   templateUrl: './layout-page.component.html',
   styles: ``
 })
 export class LayoutPageComponent {
   public sidebarItems = [
-    { label: 'Listado', icon: 'label', url: '/.list' },
-     { label: 'Añadir', icon: 'add', url: '/.new-hero' },
-     { label: 'Buscar', icon: 'search', url: '/.search' },
+    { label: 'Listado', icon: 'label', url: './list' },
+     { label: 'Añadir', icon: 'add', url: './new-hero' },
+     { label: 'Buscar', icon: 'search', url: './search' },
   ]
 
 }
